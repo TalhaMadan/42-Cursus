61adeb700ea091f7452c63a6fae04974bb058d7b
TalhaMadan <madan61.67@gmail.com>
2023-11-21 20:21:19 +0300

    change

---
+++ Exam02/Question 1/first_word/first_word.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int main(int ac ,char **str)
+{
+    int i = 0;
+
+    if (ac == 2)
+    {
+        while (str[1][i] == ' ' || str[1][i] == '\t' && str[1][i])
+            i++;
+        while (str[1][i] != ' ' && str[1][i] != '\t' && str[1][i])
+        {
+            write (1, &str[1][i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+}
